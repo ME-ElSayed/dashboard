2a2a140669aaa32ea3fce00b878f9478c242b98c
mahmoud123asd <https://github.com/mahmoud123asd>
2026-04-06 22:53:51 +0200

    Add QuickInVoice widget and integrate it into DashboardDesktopLayout

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/all_expenses.dart';
 import 'package:responsive_dashboard/widgets/custom_drawer.dart';
+import 'package:responsive_dashboard/widgets/quick_in_voice.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -11,7 +12,10 @@ class DashboardDesktopLayout extends StatelessWidget {
       children: [
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
-        Expanded(flex: 2, child: AllExpenses()),
+        Expanded(
+          flex: 2,
+          child: Column(children: [AllExpenses(), QuickInVoice()]),
+        ),
       ],
     );
   }
