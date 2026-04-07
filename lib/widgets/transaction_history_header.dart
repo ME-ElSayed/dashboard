558a1872d0d51e9ccc8a1db5401d5acda7367ab7
mahmoud123asd <https://github.com/mahmoud123asd>
2026-04-07 18:59:19 +0200

    Add TransactionModel class to represent transaction data structure

--- lib/widgets/transaction_history_header.dart
+++ lib/widgets/transaction_history_header.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/app_colors.dart';
 import 'package:responsive_dashboard/utils/app_styles.dart';
 
 class TansctionHistoryHeader extends StatelessWidget {
-  const TansctionHistoryHeader({
-    super.key,
-  });
+  const TansctionHistoryHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(
-          'Transaction History',
-          style: AppStyles.styleSemiBold20(context),
-        ),
+        Text('Transaction History', style: AppStyles.styleSemiBold20(context)),
         Text(
           'See all',
-          style: AppStyles.styleMedium16(context).copyWith(
-            color: const Color(0xFF4EB7F2),
-          ),
-        )
+          style: AppStyles.styleMedium16(
+            context,
+          ).copyWith(color: AppColors.primaryColor),
+        ),
       ],
     );
   }
