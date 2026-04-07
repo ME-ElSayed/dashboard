bafe1e54adbd8996e9fde9e72d47eb86aa106872
mahmoud123asd <https://github.com/mahmoud123asd>
2026-04-07 20:41:05 +0200

    Refactor DashboardTabletLayout to use SingleChildScrollView for better scrolling behavior; include IncomeSection and MyCardAndTransactionHistorySection

--- lib/widgets/dashboard_tablet_layout.dart
+++ lib/widgets/dashboard_tablet_layout.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/all_expenses_and_quickinvoice_section.dart';
 import 'package:responsive_dashboard/widgets/custom_drawer.dart';
+import 'package:responsive_dashboard/widgets/income_section.dart';
+import 'package:responsive_dashboard/widgets/my_card_and_transaction_history_section.dart';
 
 class DashboardTabletLayout extends StatelessWidget {
   const DashboardTabletLayout({super.key});
@@ -11,7 +13,20 @@ class DashboardTabletLayout extends StatelessWidget {
       children: [
         Expanded(child: CustomDrawer()),
         const SizedBox(width: 32),
-        Expanded(flex: 2, child: AllExpensesAndQuickInvoiceSection()),
+        Expanded(
+          flex: 2,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                AllExpensesAndQuickInvoiceSection(),
+                const SizedBox(height: 24),
+                MyCardAndTransactionHistorySection(),
+                const SizedBox(height: 24),
+                IncomeSection(),
+              ],
+            ),
+          ),
+        ),
         const SizedBox(width: 32),
       ],
     );
