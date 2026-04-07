dedbb4cdcf6b5e0b47be4224020c96bdddf76983
mahmoud123asd <https://github.com/mahmoud123asd>
2026-04-07 12:57:17 +0200

    Add AllExpensesAndQuickInvoiceSection widget; update DashboardDesktopLayout to use it

---
+++ lib/widgets/all_expenses_and_quickinvoice_section.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/all_expenses.dart';
+import 'package:responsive_dashboard/widgets/quick_in_voice.dart';
+
+class AllExpensesAndQuickInvoiceSection extends StatelessWidget {
+  const AllExpensesAndQuickInvoiceSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 40),
+          AllExpenses(),
+          const SizedBox(height: 24),
+          QuickInVoice(),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dashboard/widgets/all_expenses.dart';
+import 'package:responsive_dashboard/widgets/all_expenses_and_quickinvoice_section.dart';
 import 'package:responsive_dashboard/widgets/custom_drawer.dart';
-import 'package:responsive_dashboard/widgets/quick_in_voice.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -14,18 +13,10 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(
           flex: 2,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(height: 40),
-                AllExpenses(),
-                const SizedBox(height: 24),
-                QuickInVoice(),
-              ],
-            ),
-          ),
+          child: AllExpensesAndQuickInvoiceSection(),
         ),
       ],
     );
   }
 }
+
