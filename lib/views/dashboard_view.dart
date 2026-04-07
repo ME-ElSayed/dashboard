6c031efe41e82db6ac8527674a0bde3e79377180
mahmoud123asd <https://github.com/mahmoud123asd>
2026-04-07 21:14:07 +0200

    Refactor DashboardView to use StatefulWidget; add drawer functionality and integrate CustomDrawer

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/adaptive_layout.dart';
+import 'package:responsive_dashboard/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/widgets/dashboard_desktop_layout.dart';
 import 'package:responsive_dashboard/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dashboard/widgets/dashboard_tablet_layout.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
 
   @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: (MediaQuery.of(context).size.width < 800)
           ? AppBar(
               backgroundColor: const Color(0xFFFAFAFA),
@@ -19,12 +27,15 @@ class DashboardView extends StatelessWidget {
               surfaceTintColor: Colors.transparent,
 
               leading: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  scaffoldKey.currentState!.openDrawer();
+                },
                 icon: Icon(Icons.menu, color: Colors.black),
               ),
             )
           : null,
       backgroundColor: Color(0xfff7f9fa),
+      drawer: (MediaQuery.of(context).size.width < 800) ? CustomDrawer() : null,
       body: AdaptiveLayout(
         mobileLayout: (context) => DashboardMobileLayout(),
         tabletLayout: (context) => DashboardTabletLayout(),
