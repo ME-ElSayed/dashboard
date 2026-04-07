5d413e986cfec2fd9f85f0a64cb7232abea479a0
mahmoud123asd <https://github.com/mahmoud123asd>
2026-04-07 20:03:28 +0200

    Refactor AdaptiveLayout for improved readability; adjust tablet layout breakpoint to 1200px

--- lib/widgets/adaptive_layout.dart
+++ lib/widgets/adaptive_layout.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AdaptiveLayout extends StatelessWidget {
-  final WidgetBuilder mobileLayout,tabletLayout,desktopLayout  ;
-  const AdaptiveLayout({super.key, required this.mobileLayout, required this.tabletLayout, required this.desktopLayout});
+  final WidgetBuilder mobileLayout, tabletLayout, desktopLayout;
+  const AdaptiveLayout({
+    super.key,
+    required this.mobileLayout,
+    required this.tabletLayout,
+    required this.desktopLayout,
+  });
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxWidth < 600) {
-            return mobileLayout(context);
-          } else if (constraints.maxWidth < 900) {
-            return tabletLayout(context);
-          } else {
-            return desktopLayout(context);
-          }
-        },
-      );
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileLayout(context);
+        } else if (constraints.maxWidth < 1200) {
+          return tabletLayout(context);
+        } else {
+          return desktopLayout(context);
+        }
+      },
+    );
   }
 }
